67083d33113edd3c42db4b89fc60c80a09bd2287
Praveen K <praveenit92@gmail.com>
2025-05-11 13:34:18 +0530

    name Change

--- fgt-spa/src/pages/Header/Header.tsx
+++ fgt-spa/src/pages/Header/Header.tsx
@@ -82,7 +82,7 @@ const Header = () => {
               color: "#006cbb",
             }}
           >
-            Futuristic Global Technology
+            Futuristic Global Technologies
           </Typography>
 
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>

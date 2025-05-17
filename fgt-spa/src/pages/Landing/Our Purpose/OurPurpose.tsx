e3e34611566bc95577009f42d1c66415f92f4978
Mekala <gmekala29@gmail.com>
2025-05-17 21:22:42 +0530

    dynamicImageUpdate

--- fgt-spa/src/pages/Landing/Our Purpose/OurPurpose.tsx
+++ fgt-spa/src/pages/Landing/Our Purpose/OurPurpose.tsx
@@ -10,6 +10,7 @@ const OurPurpose = () => {
     title
     description
     id
+    imageUrl
   }
   }
 
@@ -80,7 +81,8 @@ const OurPurpose = () => {
           padding: 2,
           color: "#fff",
           position: "relative",
-          backgroundImage: 'url("https://images.unsplash.com/photo-1584438784894-089d6a62b8fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+          // backgroundImage: 'url("https://images.unsplash.com/photo-1584438784894-089d6a62b8fa?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
+              backgroundImage: `url(${purpose.imageUrl?.trim() || "https://images.unsplash.com/photo-1584438784894-089d6a62b8fa?q=80&w=2070"})`,
           transition: "0.5s ease",
           cursor: "pointer",
           "&:hover": {
